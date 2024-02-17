7c39b345c386dddc075ab55cd591fd7446d91cd9
skyikho <skyvtex@gmail.com>
2024-02-18 08:13:54 +0900

    zone list

--- ncloud/vpc/main.tf
+++ ncloud/vpc/main.tf
@@ -2,3 +2,8 @@ resource "ncloud_vpc" "vpc" {
   name            = var.vpc_name
   ipv4_cidr_block = var.vpc_cidr
 }
+
+data "ncloud_zones" "zones" {
+  region      = var.region
+  output_file = "zones.json"
+}
--- ncloud/vpc/variables.tf
+++ ncloud/vpc/variables.tf
@@ -1,3 +1,4 @@
+variable region {}
 # resource "ncloud_vpc" "vpc"
 variable vpc_name {}
 #(Optional)생성할 VPC의 이름. 생략되면 Terraform이 임의의 고유한 이름을 할당
